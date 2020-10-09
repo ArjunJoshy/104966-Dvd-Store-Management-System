2783231939371d81f46af5159f5cf4af2fa7e320
Arjun Joshy <69142123+stepin104966@users.noreply.github.com>
2020-10-09 20:17:51 +0530

    Create DvdStore.h

---
+++ 3_Implementation/inc/DvdStore.h
@@ -0,0 +1,47 @@
+#ifndef DvdStore_H_INCLUDED
+#define DvdStore_H_INCLUDED
+
+
+void addstaff(void) ;
+int checkid(int t) ;
+void delestaff() ;
+void editstaff(void) ;
+int getdata() ;
+void gotoxy (int x, int y) ;
+void mainmenu() ;
+void Password(void) ;
+void returnfunc(void) ;
+void searchstaff() ;
+int chechid(int) ;
+void viewstaff(void)  ;
+
+struct meroDate
+
+{
+
+int mm,dd,yy;
+
+};
+
+struct staff
+
+{
+
+int id;
+char stname[20];
+char name[20];
+char available[20];
+int qty;
+int price;
+int count;
+char *cat;
+struct meroDate issued;
+struct meroDate duedate;
+
+};
+
+
+
+
+
+#endif
